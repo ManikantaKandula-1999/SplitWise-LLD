b3a17071e55157b53faa7838640c5e0410f1ba57
fluence-manikanta <kandulavenkata.m@fluenceenergy.com>
2026-03-16 19:01:40 +0530

    init

---
+++ src/main/java/com/example/splitwiselld/SplitWiseLldApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitwiselld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWiseLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitWiseLldApplication.class, args);
+    }
+
+}
